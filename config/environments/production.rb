bb1d169eb06a565d1ed66de6e5eb2c49f19fcea0
Sean <sean820117@gmail.com>
2013-10-06 23:37:18 +0800

    test9

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 SampleApp::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  #loading assets
+  config.serve_static_assets = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
