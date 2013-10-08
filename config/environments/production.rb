73d325842a8bb002d68c90552c8a096559206d05
Sean <sean820117@gmail.com>
2013-10-08 22:46:16 +0800

    add assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ SampleApp::Application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security,
   # and use secure cookies.
   config.force_ssl = true
+  config.serve_static_assets = true
 end
