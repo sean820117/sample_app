bde26d3be2760663e17d0c867667ad63940df80b
Sean <sean820117@gmail.com>
2013-10-08 22:38:55 +0800

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -107,5 +107,14 @@ before do
       specify { expect(user_for_invalid_password).to be_false }
     end
   end
+
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 end
 
